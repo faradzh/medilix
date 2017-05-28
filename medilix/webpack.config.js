1d7e869145c1dc8c028ca0c7733264e2aa5c6405
faradj <faradj.musaev@gmail.com>
2017-05-28 12:26:15 +0600

    * The migrations related to all created models

--- medilix/webpack.config.js
+++ medilix/webpack.config.js
@@ -5,7 +5,7 @@ const SOURCE_DIR = path.resolve(__dirname, './api/static/js/src');
 const BUILD_DIR = path.resolve(__dirname, './api/static/js/dist');
 
 const config = {
-    entry: SOURCE_DIR + '/index.js',
+    entry: SOURCE_DIR + '/app.js',
     output: {
         path: BUILD_DIR,
         filename: '[name].bundle.js'
@@ -22,12 +22,20 @@ const config = {
                 }
             },
             {
-                 test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
-                 loader: 'url-loader?limit=8192'
+                test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=8192&name=images/[hash].[ext]'
             },
             {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader?attrs[]=video:src'
+            },
+            {
+                test: /\.mp4$/,
+                loader: 'url?limit=10000&mimetype=video/mp4'
             }
         ]
     }
