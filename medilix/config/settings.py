a5502c03f27f9e869ed091908605c62ac1b34ebe
faradj <faradj.musaev@gmail.com>
2017-05-29 23:38:49 +0600

    * Changed import statement of local_settings.py * Corrected user signup to ignore letter case in the names of groups

--- medilix/config/settings.py
+++ medilix/config/settings.py
@@ -141,6 +141,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 try:
-    from local_settings import *
+    from config.local_settings import *
 except ImportError as e:
     pass
